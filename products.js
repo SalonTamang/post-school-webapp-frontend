a02d904ae02fd753734501328b4c430baa7ae61b
salon <salon.ghalan@gmail.com>
2023-10-27 03:11:49 +0100

    Added products.js file

---
+++ products.js
@@ -0,0 +1,100 @@
+const products = [
+    {
+        id: 1,
+        title: "<b>Maths</b>",
+        location: "India",
+        price: 100,
+        image: "images/Math.jpg",
+        available:10
+    },
+    {
+        id: 2,
+        title: "<b>English</b>",
+        location: "Oxford",
+        price: 30,
+        image: "images/english.jpg",
+        available:7
+    },
+    {
+        id: 3,
+        title: "<b>Science</b>",
+        location: "Nepal",
+        price: 15,
+        image: "images/science.jpg",
+        available:15
+    },
+    {
+        id: 4,
+        title: "<b>Sports</b>",
+        location: "Spain",
+        price: 45,
+        image: "images/sport.jpg",
+        available:17
+    },
+    {
+        id: 5,
+        title: "<b>Music</b>",
+        location: "London",
+        price: 53.99,
+        image: "images/music.jpg",
+        available:5
+    },
+    {
+        id: 6,
+        title: "<b>Art</b>",
+        location: "China",
+        price: 35,
+        image: "images/art.jpg",
+        available:7
+    },
+    {
+        id: 7,
+        title: "<b>Fashion</b>",
+        location: "Geda",
+        price: 59,
+        image: "images/fashion.jpg",
+        available:10
+    },
+    {
+        id: 8,
+        title: "<b>Hair cutting</b>",
+        location: "China",
+        price: 95,
+        image: "images/hair.jpg",
+        available:9
+    },
+    {
+        id: 9,
+        title: "<b>Dance</b>",
+        location: "Canada",
+        price: 75,
+        image: "images/dance.jpg",
+        available:7
+    },
+    {
+        id: 10,
+        title: "<b>Drawing</b>",
+        location: "Polland",
+        price: 65,
+        image: "images/drawing.jpg",
+        available:8
+    },
+    {
+        id: 11,
+        title: "<b>Construction</b>",
+        location: "Hetauda",
+        price: 55,
+        image: "images/construction.jpg",
+        available:14
+    },
+    {
+        id: 12,
+        title: "<b>Health</b>",
+        location: "USA",
+        price: 105,
+        image: "images/health.jpg",
+        available:20
+    },
+];
+
+export default products;
