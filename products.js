3db525a31da905deb53bddaa69597410488f5d20
salon <salon.ghalan@gmail.com>
2024-01-27 00:50:57 +0000

    Performed some changes for express.js

---
+++ products.js
@@ -0,0 +1,112 @@
+const products = [
+    {
+        id: 1,
+        title: "<b>Maths</b>",
+        location: "Texas",
+        price: 100,
+        image: "images/Math.jpg",
+        available:10,
+        onCart:0
+    },
+    {
+        id: 2,
+        title: "<b>English</b>",
+        location: "Oxford",
+        price: 30,
+        image: "images/english.jpg",
+        available:7,
+        onCart:0
+    },
+    {
+        id: 3,
+        title: "<b>Science</b>",
+        location: "Nepal",
+        price: 15,
+        image: "images/science.jpg",
+        available:15,
+        onCart:0
+    },
+    {
+        id: 4,
+        title: "<b>Sports</b>",
+        location: "Spain",
+        price: 45,
+        image: "images/sport.jpg",
+        available:17,
+        onCart:0
+    },
+    {
+        id: 5,
+        title: "<b>Music</b>",
+        location: "London",
+        price: 53.99,
+        image: "images/music.jpg",
+        available:6,
+        onCart:0
+    },
+    {
+        id: 6,
+        title: "<b>Art</b>",
+        location: "China",
+        price: 35,
+        image: "images/art.jpg",
+        available:7,
+        onCart:0
+    },
+    {
+        id: 7,
+        title: "<b>Fashion</b>",
+        location: "Geda",
+        price: 59,
+        image: "images/fashion.jpg",
+        available:10,
+        onCart:0
+    },
+    {
+        id: 8,
+        title: "<b>Hair cutting</b>",
+        location: "China",
+        price: 95,
+        image: "images/hair.jpg",
+        available:9,
+        onCart:0
+    },
+    {
+        id: 9,
+        title: "<b>Dance</b>",
+        location: "Canada",
+        price: 75,
+        image: "images/dance.jpg",
+        available:7,
+        onCart:0
+    },
+    {
+        id: 10,
+        title: "<b>Drawing</b>",
+        location: "Polland",
+        price: 65,
+        image: "images/drawing.jpg",
+        available:8,
+        onCart:0
+    },
+    {
+        id: 11,
+        title: "<b>Construction</b>",
+        location: "Hetauda",
+        price: 55,
+        image: "images/construction.jpg",
+        available:14,
+        onCart:0
+    },
+    {
+        id: 12,
+        title: "<b>Health</b>",
+        location: "USA",
+        price: 105,
+        image: "images/health.jpg",
+        available:20,
+        onCart:0
+    },
+];
+
+export default products;
